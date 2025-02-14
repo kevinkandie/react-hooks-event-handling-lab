041f44a6a7e239089b709aae0ffc94ce101ddc0b
kevinkandie <kevinkan500@gmail.com>
2025-02-14 13:11:16 +0300

    added required files

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,7 +2,13 @@
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input
+            type ="password"
+            placeholder="Enter password"
+            onChange={()=> console.log("Entering password...")}/>
+
+        </div>
     )
 }
 
